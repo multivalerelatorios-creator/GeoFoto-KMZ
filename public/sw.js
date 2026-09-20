fe5f036def41db5265a085c2122e6273bbf6bf0e
GeoFoto KMZ <geofoto-kmz@users.noreply.github.com>
2026-09-20 13:24:47 -0300

    fix: restaurar inicializacao da camera e GPS no PWA

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='geofoto-kmz-v13';
+const CACHE='geofoto-kmz-v14';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/version.json'])))});
 self.addEventListener('message',e=>{if(e.data?.type==='SKIP_WAITING')self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
